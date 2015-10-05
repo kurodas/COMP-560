0972dcd668c6c5b28304925b9bd1984c66671b54
Shunya Kuroda <skuroda@live.unc.edu>
2015-10-05 12:34:26 -0400

    Updated Board

--- CandyGame/src/main/Board.java
+++ CandyGame/src/main/Board.java
@@ -1,46 +1,44 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import main.Cell.colors;
 
 public class Board {
 
-	public Object[][] boardArray;
+	public Cell[][] board;
 	
-	protected ArrayList<String> stringList;
-	
-	public Board(String fileLoc){
-		generateStringList(fileLoc);
-		if(!stringList.isEmpty())
-			generateBoardArray();
+	public Board(String fileLoc) throws FileNotFoundException{
+		board = new Cell[6][6];
+		generateBoard(fileLoc);
 	}
 	
-	protected ArrayList<String> generateStringList(String fileLoc){
-		BufferedReader br;
-		stringList = new ArrayList<String>(); 
-		try {
-			br = new BufferedReader (new FileReader(fileLoc) );
-			//Read in text file line by line
-			for(String input = br.readLine(); input != null; input = br.readLine()){
-				stringList.add(input);
-			}
-			br.close();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return stringList;
+	public Board(){
+		board = new Cell[6][6];
 	}
 
-	protected void generateBoardArray(){
-		boardArray = new Cell[6][6];
-		for(int y = 0; y < 6; y++){
-			String currentLine = stringList.get(y);
-			for(int x = 0; x < 6; x++){
-				
+	private void generateBoard(String fileLoc) throws FileNotFoundException{
+		Scanner input = new Scanner(new File(fileLoc));
+		for(int row = 0; row < 6; row++){
+			for(int column = 0; column < 6; column++){
+				board[row][column] = new Cell(input.nextInt(), colors.BLANK);
 			}
 		}
-	}	
+	}
+	
+	public Cell getCell(int row, int column){
+		return board[row][column];
+	}
+	
+	public Board clone(){
+		Board clone = new Board();
+		for(int row = 0; row < 6; row++){
+			for(int column = 0; column < 6; column++){
+				clone.board[row][column] = new Cell(board[row][column].value, board[row][column].color);
+			}
+		}
+		return clone;
+	}
 }
