f554e3f32a3b164b3ea2eb62118468c22f7f0734
Shunya Kuroda <skuroda@live.unc.edu>
2015-10-06 13:09:50 -0400

    Corrected play() method, added necessary private methods, added comments

--- CandyGame/src/main/Board.java
+++ CandyGame/src/main/Board.java
@@ -43,21 +43,58 @@ public class Board {
 	}
 	
 	public void play(int x, int y, Cell.colors playerColor){
-		Cell.colors oppColor = playerColor == Cell.colors.BLUE ? Cell.colors.GREEN : Cell.colors.BLUE;
 		if(board[x][y].color == Cell.colors.BLANK){
 			board[x][y].color = playerColor;
-			for(int xloop = x-1; xloop<=x+1; x++){
-				for(int yloop = y-1; yloop<=y+1; y++){
-					if(xloop >= 0 && xloop <6 && yloop >= 0 && yloop < 6){
-						if(board[xloop][yloop].color == oppColor){
-							board[xloop][yloop].color = playerColor;
-						}
-					}
-				}
+			if(nearbyCellOfSameColor(x, y, playerColor)){
+				recolorNearbyCells(x, y, playerColor);
 			}
 		}
 	}
-	
+	/**
+	 * Checks the spaces above, below, left, and right 
+	 * of the space to see if any of them are the player's color
+	 * @param x
+	 * @param y
+	 * @param playerColor
+	 * @return true if any vertically or horizontally adjacent spaces are playerColor
+	 * 			false otherwise
+	 */
+	private boolean nearbyCellOfSameColor(int x, int y, Cell.colors playerColor){
+		if(board[x - 1][y].color == playerColor)
+			return true;
+		else if(board[x + 1][y].color == playerColor)
+			return true;
+		else if(board[x][y + 1].color == playerColor)
+			return true;
+		else if (board[x][y + 1].color == playerColor)
+			return true;
+		else
+			return false;
+	}
+	/**
+	 * Recolors spacess that are above, below, left, and right
+	 * of the space at this coordinate to playerColor if 
+	 * they are the opponent's color 
+	 * @param x
+	 * @param y
+	 * @param playerColor
+	 */
+	private void recolorNearbyCells(int x, int y, Cell.colors playerColor){
+		Cell.colors oppColor = playerColor == Cell.colors.BLUE ? Cell.colors.GREEN : Cell.colors.BLUE;
+		if(board[x-1][y].color == oppColor)
+			board[x-1][y].color = playerColor;
+		if(board[x+1][y].color == oppColor)
+			board[x+1][y].color = playerColor;
+		if(board[x][y-1].color == oppColor)
+			board[x][y-1].color = playerColor;
+		if(board[x][y+1].color == oppColor)
+			board[x][y+1].color = playerColor;
+	}
+	/**
+	 * Checks to see if the game is over by looking for empty spaces 
+	 * @return true if all spaces are colored
+	 * 			false if any spaces are still uncolored
+	 */
 	public boolean isGameOver(){
 		for(int x = 0; x < 6; x++){
 			for(int y = 0; y < 6; y++){
@@ -68,7 +105,16 @@ public class Board {
 		}
 		return true;
 	}
-	
+	/**
+	 * Returns the score that the player of player color 
+	 * gets from this space
+	 * @param x
+	 * @param y
+	 * @param playerColor
+	 * @return 0 if the space is uncolored
+	 * 			the value of the space if it is the player's color
+	 * 			the negative value of the space if it is the opponent's color
+	 */
 	public int getScoreForSpace(int x, int y, Cell.colors playerColor){
 		if(board[x][y].color == Cell.colors.BLANK){
 			return 0;
